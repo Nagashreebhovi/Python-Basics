e75d8f2772932b3b87e990cf823f19114515cacc
nagashree <nagashreebhovi@gmail.com>
2026-02-24 18:28:17 +0530

    Day -1.py

---
+++ Day -1.py
@@ -0,0 +1,28 @@
+print("hello github")
+
+name="nagashree"
+print(name)
+
+student="Rashmi"
+print("Hello "+student)
+
+#using f string method
+students="nagashree and rashmi"
+print(f"name of the students {students}")
+
+name="nagu"
+age=20
+print(name)
+print(age)
+
+name="nagu"
+age=20
+print(f"name is {name} and age is {age}")
+
+
+a=10
+b=20
+print("sum=",a+b)
+
+age=20
+print("my age is",+age)
